ec27a215aa5f8624f3a40942108e8c204179203e
Kai Schulz <info@sparrowmedia.de>
2024-10-04 13:02:44 +0200

    set up canvas and first class

---
+++ script.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById('canvas');
+let character = new MovableObject();
+let ctx = canvas.getContext('2d');
+
+function init() {
+    console.log('char', character);
+}
