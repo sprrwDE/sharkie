e37174e159ac0863f9201462547e5116f8b437cc
Kai Schulz <info@sparrowmedia.de>
2024-10-12 16:10:23 +0200

    added some background styling

---
+++ scripts/classes/barrier.class.js
@@ -0,0 +1,4 @@
+class Barrier extends MovableObject {
+    // ./assets/imgs/3. Background/Barrier/2.png
+
+}
--- scripts/classes/enemy.class.js
+++ scripts/classes/enemy.class.js
@@ -3,7 +3,8 @@ class Enemy extends MovableObject {
         super().loadImage('../assets/imgs/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png')
         this.y = Math.random() * 480;
         this.x = 300 + Math.random() * 700;
-        this.height = 50 + Math.random() * 50;
+        this.height = 50 + Math.random() * 100;
         this.width = this.height;
+
     }
 }
