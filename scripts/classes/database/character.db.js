cd4935ff9e3ad536f3fce167ed0174570c0e0480
Kai Schulz <info@sparrowmedia.de>
2024-11-02 11:17:56 +0100

    v1.0

--- scripts/classes/database/character.db.js
+++ scripts/classes/database/character.db.js
@@ -36,7 +36,19 @@ class CharacterDB {
         './assets/imgs/1.Sharkie/1.IDLE/2.png',
         './assets/imgs/1.Sharkie/1.IDLE/3.png',
         './assets/imgs/1.Sharkie/1.IDLE/4.png',
-        //...
+        './assets/imgs/1.Sharkie/1.IDLE/5.png',
+        './assets/imgs/1.Sharkie/1.IDLE/6.png',
+        './assets/imgs/1.Sharkie/1.IDLE/7.png',
+        './assets/imgs/1.Sharkie/1.IDLE/8.png',
+        './assets/imgs/1.Sharkie/1.IDLE/9.png',
+        './assets/imgs/1.Sharkie/1.IDLE/10.png',
+        './assets/imgs/1.Sharkie/1.IDLE/11.png',
+        './assets/imgs/1.Sharkie/1.IDLE/12.png',
+        './assets/imgs/1.Sharkie/1.IDLE/13.png',
+        './assets/imgs/1.Sharkie/1.IDLE/14.png',
+        './assets/imgs/1.Sharkie/1.IDLE/15.png',
+        './assets/imgs/1.Sharkie/1.IDLE/16.png',
+        './assets/imgs/1.Sharkie/1.IDLE/17.png',
         './assets/imgs/1.Sharkie/1.IDLE/18.png'
     ];
 
@@ -47,7 +59,17 @@ class CharacterDB {
     IMAGES_SNOOZE = [
         './assets/imgs/1.Sharkie/2.Long_IDLE/i1.png',
         './assets/imgs/1.Sharkie/2.Long_IDLE/I2.png',
-        //...
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I3.png',
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I4.png',
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I5.png',
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I6.png',
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I7.png',
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I8.png',
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I9.png',
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I10.png',
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I11.png',
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I12.png',
+        './assets/imgs/1.Sharkie/2.Long_IDLE/I13.png',
         './assets/imgs/1.Sharkie/2.Long_IDLE/I14.png'
     ];
 
@@ -58,7 +80,9 @@ class CharacterDB {
     IMAGES_SWIM = [
         './assets/imgs/1.Sharkie/3.Swim/1.png',
         './assets/imgs/1.Sharkie/3.Swim/2.png',
-        //...
+        './assets/imgs/1.Sharkie/3.Swim/3.png',
+        './assets/imgs/1.Sharkie/3.Swim/4.png',
+        './assets/imgs/1.Sharkie/3.Swim/5.png',
         './assets/imgs/1.Sharkie/3.Swim/6.png'
     ];
 
@@ -68,7 +92,12 @@ class CharacterDB {
      */
     IMAGES_FINSLAP = [
         './assets/imgs/1.Sharkie/4.Attack/Fin slap/1.png',
-        //...
+        './assets/imgs/1.Sharkie/4.Attack/Fin slap/2.png',
+        './assets/imgs/1.Sharkie/4.Attack/Fin slap/3.png',
+        './assets/imgs/1.Sharkie/4.Attack/Fin slap/4.png',
+        './assets/imgs/1.Sharkie/4.Attack/Fin slap/5.png',
+        './assets/imgs/1.Sharkie/4.Attack/Fin slap/6.png',
+        './assets/imgs/1.Sharkie/4.Attack/Fin slap/7.png',
         './assets/imgs/1.Sharkie/4.Attack/Fin slap/8.png'
     ];
 
@@ -78,7 +107,12 @@ class CharacterDB {
      */
     IMAGES_SHOOTING = [
         './assets/imgs/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/1.png',
-        //...
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/2.png',
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/3.png',
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/4.png',
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/5.png',
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/6.png',
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/7.png',
         './assets/imgs/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/8.png'
     ];
 
@@ -88,7 +122,12 @@ class CharacterDB {
      */
     IMAGES_POISONBUBBLE = [
         './assets/imgs/1.Sharkie/4.Attack/Bubble trap/For Whale/1.png',
-        //...
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/For Whale/2.png',
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/For Whale/3.png',
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/For Whale/4.png',
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/For Whale/5.png',
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/For Whale/6.png',
+        './assets/imgs/1.Sharkie/4.Attack/Bubble trap/For Whale/7.png',
         './assets/imgs/1.Sharkie/4.Attack/Bubble trap/For Whale/8.png'
     ];
 
@@ -98,7 +137,9 @@ class CharacterDB {
      */
     IMAGES_POISONED = [
         './assets/imgs/1.Sharkie/5.Hurt/1.Poisoned/1.png',
-        //...
+        './assets/imgs/1.Sharkie/5.Hurt/1.Poisoned/2.png',
+        './assets/imgs/1.Sharkie/5.Hurt/1.Poisoned/3.png',
+        './assets/imgs/1.Sharkie/5.Hurt/1.Poisoned/4.png',
         './assets/imgs/1.Sharkie/5.Hurt/1.Poisoned/5.png'
     ];
 
@@ -108,8 +149,8 @@ class CharacterDB {
      */
     IMAGES_SHOCK = [
         './assets/imgs/1.Sharkie/5.Hurt/2.Electric shock/1.png',
-        //...
-        './assets/imgs/1.Sharkie/5.Hurt/2.Electric shock/3.png'
+        './assets/imgs/1.Sharkie/5.Hurt/2.Electric shock/2.png',
+        './assets/imgs/1.Sharkie/5.Hurt/2.Electric shock/3.png',
     ];
 
     /**
@@ -118,7 +159,16 @@ class CharacterDB {
      */
     IMAGES_DEAD = [
         './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00000.png',
-        //...
+        './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00001.png',
+        './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00002.png',
+        './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00003.png',
+        './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00004.png',
+        './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00005.png',
+        './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00006.png',
+        './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00007.png',
+        './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00008.png',
+        './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00009.png',
+        './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00010.png',
         './assets/imgs/1.Sharkie/6.dead/1.Poisoned/sin subir/DES 2_00011.png'
     ];
 
@@ -128,7 +178,14 @@ class CharacterDB {
      */
     IMAGES_DEAD_SHOCK = [
         './assets/imgs/1.Sharkie/6.dead/2.Electro_shock/1.png',
-        //...
+        './assets/imgs/1.Sharkie/6.dead/2.Electro_shock/2.png',
+        './assets/imgs/1.Sharkie/6.dead/2.Electro_shock/3.png',
+        './assets/imgs/1.Sharkie/6.dead/2.Electro_shock/4.png',
+        './assets/imgs/1.Sharkie/6.dead/2.Electro_shock/5.png',
+        './assets/imgs/1.Sharkie/6.dead/2.Electro_shock/6.png',
+        './assets/imgs/1.Sharkie/6.dead/2.Electro_shock/7.png',
+        './assets/imgs/1.Sharkie/6.dead/2.Electro_shock/8.png',
+        './assets/imgs/1.Sharkie/6.dead/2.Electro_shock/9.png',
         './assets/imgs/1.Sharkie/6.dead/2.Electro_shock/10.png'
     ];
 
